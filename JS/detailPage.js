25b35583fdbb0ad691c0d7b51469bd1f27fa287e
Lee_giun <sunday6602@naver.com>
2023-06-19 17:23:43 +0900

    '13_0619'

--- JS/detailPage.js
+++ JS/detailPage.js
@@ -294,8 +294,8 @@ ColorSelectAll.forEach(label => {
   
   ThumbIMG.forEach((element, index) => {
     element.addEventListener('click', function() {
-      DetailThumbIMG.style.backgroundImage = `url(./BACK_IMG/detail${index + 1}.jpeg)`
-/*       DetailThumbIMG.style.backgroundImage = `url(../CSS/BACK_IMG/detail${index + 1}.jpeg)` */
+      // DetailThumbIMG.style.backgroundImage = `url(./BACK_IMG/detail${index + 1}.jpeg)`
+      DetailThumbIMG.style.backgroundImage = `url(../CSS/BACK_IMG/detail${index + 1}.jpeg)`
     }) // Git에 올릴때는 아래 // parcel-bundler로 할때는 위 사용
   })
 
